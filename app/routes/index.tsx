aa8f6732b0b9ca2be33379fe36d53299ecc9a29e
Wycliff Ogembo <wycliffon@gmail.com>
2022-07-14 11:57:45 +0300

    Switched to mantime css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,49 +1,108 @@
-import { Text, Container, Box, VStack } from "@chakra-ui/react"
+import React from 'react';
+import { Link } from "@remix-run/react";
+import { createStyles, Overlay, Container, Title, Button, Text, Navbar } from '@mantine/core';
 
-function Slogan() {
-  return (
-    <Box alignItems="center" w="full" h="full" p={5} boxShadow="2xl">
-      <VStack w='100%' h="100%">
-        <Text fontSize={50} textAlign="center" textColor="black">Welcome to Ziidoh</Text>
-        <Text fontSize={25} textColor="blue.500">The friendly teams network</Text>
-      </VStack>
-    </Box>
-  )
-}
+import different_sports_image from "../images/friendlies.jpeg";
 
-export default function Index() {
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    height: 75,
+    position: 'fixed',
+    backgroundColor: theme.white,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: '20px'
+  },
+
+  name: {
+    fontSize: 20,
+    fontWeight: 900
+  },
+
+  hero: {
+    position: 'relative',
+    backgroundImage: `url(${different_sports_image})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    margin: '-10px'
+  },
+
+  container: {
+    height: 700,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingBottom: theme.spacing.xl * 6,
+    zIndex: 1,
+    position: 'relative',
+
+    [theme.fn.smallerThan('sm')]: {
+      height: 500,
+      paddingBottom: theme.spacing.xl * 3,
+    },
+  },
+
+  title: {
+    color: theme.white,
+    fontSize: 60,
+    fontWeight: 900,
+    lineHeight: 1.1,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 40,
+      lineHeight: 1.2,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 28,
+      lineHeight: 1.3,
+    },
+  },
+
+  description: {
+    color: theme.white,
+    maxWidth: 600,
+
+    [theme.fn.smallerThan('sm')]: {
+      maxWidth: '100%',
+      fontSize: theme.fontSizes.sm,
+    },
+  },
+
+  control: {
+    marginTop: theme.spacing.xl * 1.5,
+
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%',
+    },
+  },
+}));
+
+export default function HeroContentLeft() {
+  const { classes } = useStyles();
+
   return (
-    <Container w="screen" h="screen" padding={5} centerContent>
-      <Slogan />
-    </Container>
+    <div className={classes.hero}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+      <Navbar className={classes.navbar}>
+        <Text className={classes.name}>FriendlyMatcher</Text>
+        <Link prefetch="intent" to="/login">Login</Link>
+      </Navbar>
+      <Container className={classes.container}>
+        <Title className={classes.title}>Easily get a team to play a friendly match with</Title>
+        <Text className={classes.description} size="xl" mt="xl">
+          We have a database of many sports teams ready to play friendly matches with your team
+        </Text>
+        <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
+          Find a team
+        </Button>
+      </Container>
+    </div>
   );
 }
-  //   <Center bg='tomato' h='100px' color='white'>
-  //     <h1>Welcome to Remix</h1>
-  //     <ul>
-  //       <li>
-  //         <a
-  //           target="_blank"
-  //           href="https://remix.run/tutorials/blog"
-  //           rel="noreferrer"
-  //         >
-  //           15m Quickstart Blog Tutorial
-  //         </a>
-  //       </li>
-  //       <li>
-  //         <a
-  //           target="_blank"
-  //           href="https://remix.run/tutorials/jokes"
-  //           rel="noreferrer"
-  //         >
-  //           Deep Dive Jokes App Tutorial
-  //         </a>
-  //       </li>
-  //       <li>
-  //         <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-  //           Remix Docs
-  //         </a>
-  //       </li>
-  //     </ul>
-  //   </Center>
-  // );
