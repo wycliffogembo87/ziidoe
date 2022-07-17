16d5b85de66644217c71c5a7728c966f8928e2fb
jeffongoro <ongorogc@gmail.com>
2022-07-17 09:02:28 +0300

    remove vertical scroll bar and login link on the navigation section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,44 +1,52 @@
-import React from 'react';
+import React from "react";
 import { Link } from "@remix-run/react";
-import { createStyles, Overlay, Container, Title, Button, Text, Navbar } from '@mantine/core';
+import {
+  createStyles,
+  Overlay,
+  Container,
+  Title,
+  Button,
+  Text,
+  Navbar,
+} from "@mantine/core";
 
-import different_sports_image from "../images/friendlies.jpeg";
+import different_sports_image from "../images/landingpage.jpg";
 
 const useStyles = createStyles((theme) => ({
   navbar: {
     height: 75,
-    position: 'fixed',
-    backgroundColor: theme.white,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: '20px'
+    position: "fixed",
+    backgroundColor: theme.black,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "20px",
   },
 
   name: {
     fontSize: 20,
-    fontWeight: 900
+    fontWeight: 900,
   },
 
   hero: {
-    position: 'relative',
+    position: "relative",
     backgroundImage: `url(${different_sports_image})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    margin: '-10px'
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    margin: "-10px",
   },
 
   container: {
-    height: 700,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
+    height: 450,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
     paddingBottom: theme.spacing.xl * 6,
     zIndex: 1,
-    position: 'relative',
+    position: "relative",
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       height: 500,
       paddingBottom: theme.spacing.xl * 3,
     },
@@ -50,12 +58,12 @@ const useStyles = createStyles((theme) => ({
     fontWeight: 900,
     lineHeight: 1.1,
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       fontSize: 40,
       lineHeight: 1.2,
     },
 
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan("xs")]: {
       fontSize: 28,
       lineHeight: 1.3,
     },
@@ -65,8 +73,8 @@ const useStyles = createStyles((theme) => ({
     color: theme.white,
     maxWidth: 600,
 
-    [theme.fn.smallerThan('sm')]: {
-      maxWidth: '100%',
+    [theme.fn.smallerThan("sm")]: {
+      maxWidth: "100%",
       fontSize: theme.fontSizes.sm,
     },
   },
@@ -74,8 +82,8 @@ const useStyles = createStyles((theme) => ({
   control: {
     marginTop: theme.spacing.xl * 1.5,
 
-    [theme.fn.smallerThan('sm')]: {
-      width: '100%',
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
     },
   },
 }));
@@ -91,16 +99,27 @@ export default function HeroContentLeft() {
         zIndex={0}
       />
       <Navbar className={classes.navbar}>
-        <Text className={classes.name}>FriendlyMatcher</Text>
-        <Link prefetch="intent" to="/login">Login</Link>
+        <Text color="white" className={classes.name}>
+          FriendlyMatcher
+        </Text>
       </Navbar>
       <Container className={classes.container}>
-        <Title className={classes.title}>Easily get a team to play a friendly match with</Title>
+        <Title className={classes.title}>
+          Find an opponent of your choice for your next friendly match
+        </Title>
         <Text className={classes.description} size="xl" mt="xl">
-          We have a database of many sports teams ready to play friendly matches with your team
+          We have a database of many sports teams ready to play friendly matches
+          with your team
         </Text>
-        <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
-          Find a team
+        <Button
+          variant="gradient"
+          size="xl"
+          radius="xl"
+          className={classes.control}
+          component={Link}
+          to="/login"
+        >
+          Schedule Match
         </Button>
       </Container>
     </div>
